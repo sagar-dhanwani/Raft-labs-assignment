84ef7f19e21f5346c5758afb209a7f7744ca6640
Sagar Dhanwani <19ce023.sagar.dhanwani@vvpedulink.ac.in>
2025-01-27 23:45:58 +0530

    feat:added swagger and docker compose file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import cors from 'cors';
+import { setupSwagger } from './config/swagger';
 
-const createApp = (): express.Application => {
+const createApp = () => {
   const app = express();
   app.use(cors());
   app.use(json());
 
+  // Setup Swagger
+  setupSwagger(app);
+
   return app;
 };
 
-export default createApp
+export default createApp;
